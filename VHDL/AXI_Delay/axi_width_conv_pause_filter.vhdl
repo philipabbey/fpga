515afa0b0a07616c463320c87f49ad580273442a
Philip Abbey <philipabbey@users.noreply.github.com>
2023-03-11 14:10:28 +0000

    Update axi_width_conv_pause_filter.vhdl
    Changed the case statement decoding for readability.

--- VHDL/AXI_Delay/axi_width_conv_pause_filter.vhdl
+++ VHDL/AXI_Delay/axi_width_conv_pause_filter.vhdl
@@ -31,15 +31,6 @@ end entity;
 
 architecture rtl of axi_width_conv_pause_filter is
 
-  -- 's_axi_byte_en(1:0)'
-  --
-  -- | 1:0 | Actions
-  -- +-----+--------------------------------------------------------
-  -- | 0 0 | Emit an invalid data cycle
-  -- | 0 1 | Emit low byte and move on to next input word
-  -- | 1 0 | Emit high byte and move on to next input word
-  -- | 1 1 | Emit low, then high byte and move on to next input word
-
   -- We're processing the first half of the input word and hence about to process the second half
   signal s_half : std_logic := '0';
 
@@ -53,42 +44,52 @@ begin
     if rising_edge(clk) then
 
       if m_axi_ready = '1' then
-        case to_bitvector(s_axi_byte_en & s_half) is
 
-          when "000" | "001" =>
+        -- 's_axi_byte_en(1:0)'
+        --
+        -- | 1:0 | Actions
+        -- +-----+--------------------------------------------------------
+        -- | 0 0 | Emit an invalid data cycle
+        -- | 0 1 | Emit low byte and move on to next input word
+        -- | 1 0 | Emit high byte and move on to next input word
+        -- | 1 1 | Emit low, then high byte and move on to next input word
+        case to_bitvector(s_axi_byte_en) is
+
+          when "00" =>
             m_axi_valid <= '0';
             if m_axi_valid = '1' then
               s_half <= '0';
             end if;
 
-          when "010" | "011" =>
+          when "01" =>
             m_axi_data  <= s_axi_data(7 downto 0);
             m_axi_valid <= s_axi_valid and enable;
             if m_axi_valid = '1' then
               s_half <= '0';
             end if;
 
-          when "100" | "101" =>
+          when "10" =>
             m_axi_data  <= s_axi_data(15 downto 8);
             m_axi_valid <= (s_axi_valid or s_half) and enable;
             if m_axi_valid = '1' then
               s_half <= '0';
             end if;
 
-          when "110" =>
-            m_axi_data  <= s_axi_data(7 downto 0);
-            m_axi_valid <= s_axi_valid and enable;
-            if s_axi_valid = '1' and enable = '1' then
-              s_half <= '1';
-            end if;
-
-          -- This is where we are about to process the second half of a word where both bytes are valid
-          when "111" =>
-            m_axi_data  <= s_axi_data(15 downto 8);
-            m_axi_valid <= '1';
-            -- 'enable' is omitted as we're pausing the input only, and finishing the output
-            if m_axi_valid = '1' then
-              s_half <= '0';
+          when "11" =>
+            if s_half = '1' then
+              -- This is where we are about to process the second half of a word where both bytes are valid
+              m_axi_data  <= s_axi_data(15 downto 8);
+              m_axi_valid <= '1';
+              if m_axi_valid = '1' then
+                -- 'enable' is omitted as we're pausing the input only, and finishing the output
+                s_half <= '0';
+              end if;
+            else
+              m_axi_data  <= s_axi_data(7 downto 0);
+              m_axi_valid <= s_axi_valid and enable;
+              if s_axi_valid = '1' and enable = '1' then
+                s_half <= '1';
+              end if;
             end if;
 
         end case;
